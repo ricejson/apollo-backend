2c435f3e2178b1b0bda7e0517abb413e2d1a3a27
codeywang <codeywang@didiglobal.com>
2026-01-25 15:50:54 +0800

    提交go sdk测试rpc接口代码

---
+++ mock/client/golang/client.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"github.com/ricejson/apollo-backend/controller/grpc/proto"
+	"google.golang.org/grpc"
+	"log"
+)
+
+func main() {
+	// 生成连接对象
+	conn, err := grpc.Dial("localhost:8992", grpc.WithInsecure())
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+	// 获取客户端
+	client := proto.NewRPCToggleServiceClient(conn)
+	insertOne, err := client.InsertOne(context.Background(), &proto.InsertOneReq{
+		Toggle: &proto.Toggle{
+			Id:          "1",
+			Name:        "test_toggle",
+			Key:         "gs_test_toggle",
+			Description: "test toggle description",
+			Status:      "",
+			Audiences: []*proto.Audience{
+				{
+					Id:   "1",
+					Name: "白名单",
+					Rules: []*proto.Rule{
+						{
+							Id:        "1",
+							Attribute: "user_id",
+							Operator:  "=",
+							Value:     "1",
+						},
+					},
+				},
+			},
+		},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("InsertOne:%v", insertOne)
+
+}
